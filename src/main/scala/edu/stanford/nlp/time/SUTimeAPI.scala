ac3b86289667fc83706bc3670d73d316b974799f
Mihai Surdeanu <surdeanu@gmail.com>
2021-01-27 20:15:17 -0700

    started scala API

---
+++ src/main/scala/edu/stanford/nlp/time/SUTimeAPI.scala
@@ -0,0 +1,16 @@
+
+package edu.stanford.nlp.time
+
+import edu.stanford.nlp.ie.regexp.NumberSequenceClassifier
+
+class SUTimeAPI {
+
+}
+
+object SUTimeAPI {
+  def main(args: Array[String]): Unit = {
+    val numSeqClassifier = new NumberSequenceClassifier()
+
+
+  }
+}
